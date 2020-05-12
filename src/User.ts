1eea1ef4d2b996bc186fa6f1eaa408c767e08d1d
Mike Hockerman <mike@hockerman.com>
2020-05-12 15:50:56 -0400

    Patch some issues surrounding string interpretation from the CSV. Update dependent packages.

--- src/User.ts
+++ src/User.ts
@@ -227,54 +227,54 @@ export class User {
       accessKey1LastUsedRegion, accessKey1LastUsedService, accessKey2Active, accessKey2LastRotated,
       accessKey2LastUsed, accessKey2LastUsedRegion, accessKey2LastUsedService, cert1Active,
       cert1LastRotated, cert2Active, cert2LastRotated ] = csvRow;
-    this.accessKey1Active = accessKey1Active === "TRUE";
-    this.accessKey1LastRotated = accessKey1LastRotated !== "N/A"
+    this.accessKey1Active = accessKey1Active.toLowerCase() === "true";
+    this.accessKey1LastRotated = accessKey1LastRotated.toLowerCase() !== "n/a"
       ? new Date(accessKey1LastRotated)
       : undefined;
-    this.accessKey1LastUsed = accessKey1LastUsed !== "N/A"
+    this.accessKey1LastUsed = accessKey1LastUsed.toLowerCase() !== "n/a"
       ? new Date(accessKey1LastUsed)
       : undefined;
-    this.accessKey1LastUsedRegion = accessKey1LastUsedRegion !== "N/A"
+    this.accessKey1LastUsedRegion = accessKey1LastUsedRegion.toLowerCase() !== "n/a"
       ? accessKey1LastUsedRegion
       : undefined;
-    this.accessKey1LastUsedService = accessKey1LastUsedService !== "N/A"
+    this.accessKey1LastUsedService = accessKey1LastUsedService.toLowerCase() !== "n/a"
       ? accessKey1LastUsedService
       : undefined;
-    this.accessKey2Active = accessKey2Active === "TRUE";
-    this.accessKey2LastRotated = accessKey2LastRotated !== "N/A"
+    this.accessKey2Active = accessKey2Active.toLowerCase() === "true";
+    this.accessKey2LastRotated = accessKey2LastRotated.toLowerCase() !== "n/a"
       ? new Date(accessKey2LastRotated)
       : undefined;
-    this.accessKey2LastUsed = accessKey2LastUsed !== "N/A"
+    this.accessKey2LastUsed = accessKey2LastUsed.toLowerCase() !== "n/a"
       ? new Date(accessKey2LastUsed)
       : undefined;
-    this.accessKey2LastUsedRegion = accessKey2LastUsedRegion !== "N/A"
+    this.accessKey2LastUsedRegion = accessKey2LastUsedRegion.toLowerCase() !== "n/a"
       ? accessKey2LastUsedRegion
       : undefined;
-    this.accessKey2LastUsedService = accessKey2LastUsedService !== "N/A"
+    this.accessKey2LastUsedService = accessKey2LastUsedService.toLowerCase() !== "n/a"
       ? accessKey2LastUsedService
       : undefined;
     this.arn = arn;
-    this.cert1Active = cert1Active === "TRUE";
-    this.cert1LastRotated = cert1LastRotated !== "N/A"
+    this.cert1Active = cert1Active.toLowerCase() === "true";
+    this.cert1LastRotated = cert1LastRotated.toLowerCase() !== "n/a"
       ? new Date(cert1LastRotated)
       : undefined;
-    this.cert2Active = cert2Active === "TRUE";
-    this.cert2LastRotated = cert2LastRotated !== "N/A"
+    this.cert2Active = cert2Active.toLowerCase() === "true";
+    this.cert2LastRotated = cert2LastRotated.toLowerCase() !== "n/a"
       ? new Date(cert2LastRotated)
       : undefined;
     this.creationTime = new Date(userCreationTime);
     this.isRoot = username === "<root_account>";
-    this.mfaActive = mfaActive === "TRUE";
+    this.mfaActive = mfaActive.toLowerCase() === "true";
     this.passwordEnabled = this.isRoot
       ? true
-      : passwordEnabled === "TRUE";
-    this.passwordLastChanged = passwordLastChanged !== "N/A" && passwordLastChanged !== "not_supported"
+      : passwordEnabled.toLowerCase() === "true";
+    this.passwordLastChanged = passwordLastChanged.toLowerCase() !== "n/a" && passwordLastChanged.toLowerCase() !== "not_supported"
       ? new Date(passwordLastChanged)
       : undefined;
-    this.passwordLastUsed = passwordLastUsed !== "N/A" && passwordLastUsed !== "no_information"
+    this.passwordLastUsed = passwordLastUsed.toLowerCase() !== "n/a" && passwordLastUsed.toLowerCase() !== "no_information"
       ? new Date(passwordLastUsed)
       : undefined;
-    this.passwordNextRotation = passwordNextRotation !== "N/A" && passwordNextRotation !== "not_supported"
+    this.passwordNextRotation = passwordNextRotation.toLowerCase() !== "n/a" && passwordNextRotation.toLowerCase() !== "not_supported"
       ? new Date(passwordNextRotation)
       : undefined;
     this.username = username;
